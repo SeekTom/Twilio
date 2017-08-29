ca329c666e6110c8721eff81e468c4026b072aef
Thomas Curtis <thomas.e.curtis@gmail.com>
2017-08-29 14:48:28 +0100

    f
    f

---
+++ Taskrouter_alt/ACMECorp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcmecorpConfig(AppConfig):
+    name = 'ACMECorp'
---
+++ Taskrouter_alt/Taskrouter_alt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Taskrouter_alt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Taskrouter_alt.settings")
+
+application = get_wsgi_application()
